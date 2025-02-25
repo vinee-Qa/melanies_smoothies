242adfc3200b05034d935a0d1b3bf14bf432d4d9
vinee-Qa <75550981+vinee-Qa@users.noreply.github.com>
2025-02-25 14:40:46 +1100

    Update streamlit_app.py
    Added subheader to fix watermelon appearing in the dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,15 +30,16 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+        st.subheader(fruit_chosen + 'Nutrition Information')
         smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
-    st.write(ingredients_string)
+   # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
     values ('""" + ingredients_string + """','""" +name_on_order+ """')"""
 
-    st.write(my_insert_stmt)
+   # st.write(my_insert_stmt)
 
     time_to_start = st.button('Submit Order')
     
