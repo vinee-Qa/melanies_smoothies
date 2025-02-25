6fc0a30c921b6c6902cdaddb65f84963ed441c97
vinee-Qa <75550981+vinee-Qa@users.noreply.github.com>
2025-02-25 14:20:30 +1100

    Update streamlit_app.py
    Added json data inside the response

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,6 +48,7 @@ if ingredients_list:
 
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
 st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json())
 
 
         
