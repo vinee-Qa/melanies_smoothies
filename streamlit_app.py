f37441f1b069db242826166c174f017f920c337f
vinee-Qa <75550981+vinee-Qa@users.noreply.github.com>
2025-02-25 14:18:40 +1100

    Update streamlit_app.py
    Added library to call smoothie froot app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake")
 session = cnx.session()
@@ -44,6 +45,11 @@ if ingredients_list:
     if time_to_start:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered'","+ name_on_order + '!', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
+
+
         
 
     
